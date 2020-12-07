5f8108c4aa6626afd4b601af8e60c2271621aa39
Your Name <mmuzasantuna@gmail.com>
2020-12-07 15:16:41 +0000

    can render listLetters held on state

--- src/containers/LetterConsole.js
+++ src/containers/LetterConsole.js
@@ -4,15 +4,20 @@ import LetterBox from '../components/LetterBox'
 const LetterConsole = () => {
     // const [letters, setLetters] = useState(0)
 
-    const letters = [
+    let [letters, setLetters] = useState([
         {author:"Maria", message: "Kaixo Olentzero"},
         {author:"Gabriel", message: "Hello Santa"},
-        {author:"Alvaro", message: "Hola Reyes Magos"}]
+        {author:"Alvaro", message: "Hola Reyes Magos"}])
+
+    // const change = () => {
+    //     setLetters[2].message( "No creo en Santa")
+    // }
 
     return(
         <>
         <h2> This is the letter console</h2>
         <LetterBox letters={letters}/>
+        {/* <button onClick = {change}> Grinch </button> */}
         </>
     )
 }
