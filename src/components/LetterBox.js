1dce71e992ad8e57486aaa4b88260c8540183637
Your Name <mmuzasantuna@gmail.com>
2020-12-07 14:10:01 +0000

    mistake in previous commit: letterConsole created. This commit letterBox component created

---
+++ src/components/LetterBox.js
@@ -0,0 +1,7 @@
+const LetterBox = () => {
+    return(
+        <h3>LetterBox component</h3>
+    )
+}
+
+export default LetterBox;
--- src/containers/LetterConsole.js
+++ src/containers/LetterConsole.js
@@ -1,10 +1,14 @@
 import {useState} from 'react';
+import LetterBox from '../components/LetterBox'
 
 const LetterConsole = () => {
     const [letters, setLetters] = useState(0)
 
     return(
+        <>
         <h2> This is the letter console</h2>
+        <LetterBox />
+        </>
     )
 }
 
