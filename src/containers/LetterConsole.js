1802f98ad2f0219a045e443f3225efa88f49fa8c
Your Name <mmuzasantuna@gmail.com>
2020-12-07 14:03:05 +0000

    app working with LetterBox cointener created and working

---
+++ src/containers/LetterConsole.js
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+
+const LetterConsole = () => {
+    const [letters, setLetters] = useState(0)
+
+    return(
+        <h2> This is the letter console</h2>
+    )
+}
+
+export default LetterConsole;
